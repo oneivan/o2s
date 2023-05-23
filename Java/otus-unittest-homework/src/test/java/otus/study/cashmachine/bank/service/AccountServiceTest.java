0dc02391ec8ba8e4ea534ca286ce9e91579f8094
oneivan <enekbenek@gmail.com>
2023-05-23 17:52:21 +0300

    Homework branch AS-IS

---
+++ Java/otus-unittest-homework/src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
@@ -0,0 +1,40 @@
+package otus.study.cashmachine.bank.service;
+
+
+import org.junit.jupiter.api.Test;
+import otus.study.cashmachine.bank.dao.AccountDao;
+import otus.study.cashmachine.bank.service.impl.AccountServiceImpl;
+
+
+public class AccountServiceTest {
+
+    AccountDao accountDao;
+
+    AccountServiceImpl accountServiceImpl;
+
+    @Test
+    void createAccountMock() {
+// @TODO test account creation with mock and ArgumentMatcher
+    }
+
+    @Test
+    void createAccountCaptor() {
+//  @TODO test account creation with ArgumentCaptor
+    }
+
+    @Test
+    void addSum() {
+    }
+
+    @Test
+    void getSum() {
+    }
+
+    @Test
+    void getAccount() {
+    }
+
+    @Test
+    void checkBalance() {
+    }
+}
